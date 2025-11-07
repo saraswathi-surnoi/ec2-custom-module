b053e1a4104d0414b57c64440e074db10ba40a41
YourUsername <saraswathinandula@surnoi.com>
2025-11-07 10:51:13 +0530

    made changes

--- surnoi-ec2_module_ssm_paramater/customize-ec2-module/ec2-module/main.tf
+++ surnoi-ec2_module_ssm_paramater/customize-ec2-module/ec2-module/main.tf
@@ -1,26 +1,34 @@
-
 resource "aws_instance" "ec2_instances" {
   for_each = var.instances
 
-  ami                         = var.ami_id
-  instance_type               = each.value.instance_type
-  subnet_id                   = var.subnet_id
-  key_name                    = var.key_pair_name
-  vpc_security_group_ids      = [lookup(var.sg_map, each.value.security_group_ref)]
-  iam_instance_profile        = lookup(each.value, "iam_instance_profile", null)
-  user_data                   = lookup(each.value, "user_data", null) != null ? file(each.value.user_data) : null
+  ami                    = var.ami_id
+  instance_type          = each.value.instance_type
+  subnet_id              = var.subnet_id
+  key_name               = var.key_pair_name
+  vpc_security_group_ids = [lookup(var.sg_map, each.value.security_group_ref)]
 
+ 
+  iam_instance_profile = lookup(each.value, "iam_instance_profile", null)
+
+  
+  user_data = lookup(each.value, "user_data", null) != null ? file("${path.root}/${each.value.user_data}") : null
+
+  
   root_block_device {
     volume_size = each.value.volume_size
     volume_type = lookup(each.value, "volume_type", "gp3")
   }
 
-  tags = merge(var.common_tags, {
-    Name        = "${var.project_name}-${var.environment}-${each.key}"
-    Project     = var.project_name
-    Environment = var.environment
-  })
+  tags = merge(
+    var.common_tags,
+    {
+      Name        = "${var.project_name}-${var.environment}-${each.key}"
+      Project     = var.project_name
+      Environment = var.environment
+    }
+  )
 }
+
 
 
 resource "aws_route53_record" "dns_records" {
--- surnoi-ec2_module_ssm_paramater/customize-ec2-module/ec2-module/output.tf
+++ surnoi-ec2_module_ssm_paramater/customize-ec2-module/ec2-module/output.tf
@@ -1,15 +1,14 @@
 output "instance_public_ips" {
   description = "Public IPs of EC2 instances"
-  value       = { for k, v in aws_instance.ec2_instances : k => v.public_ip }
+  value       = { for k, inst in aws_instance.ec2_instances : k => inst.public_ip }
 }
+
 output "instance_private_ips" {
   description = "Private IPs of EC2 instances"
-  value       = { for k, v in aws_instance.ec2_instances : k => v.private_ip }
+  value       = { for k, inst in aws_instance.ec2_instances : k => inst.private_ip }
 }
-output "dns_records" {
-  description = "DNS names of instances (if created)"
-  value = var.create_dns ? {
-    for k, v in aws_instance.ec2_instances :
-    k => "${k}.${var.project_name}.${var.environment}.${var.domain_name}"
-  } : {}
+
+output "instance_ids" {
+  description = "Instance IDs of created EC2s"
+  value       = { for k, inst in aws_instance.ec2_instances : k => inst.id }
 }
